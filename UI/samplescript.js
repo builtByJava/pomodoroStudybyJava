abd28290e768b332d23c5a556a5d854f770df86f
BlackRain29 <55949931+BlackRain29@users.noreply.github.com>
2023-08-09 20:32:02 -0700

    fixed logic bugs with clock functionality in regards to timer skipping seconds, restart/resume performance, and break clock

--- UI/samplescript.js
+++ UI/samplescript.js
@@ -1,8 +1,10 @@
 //variables
-let seconds = 0;
+let seconds = 1500; //default Pomodoro timer session of 25min increments
 let interval = null;
 let totalPSessions = 0;
 let sessionCount = 0;
+let count = 0;
+let storedSeconds = 0;
 
 const startButton = document.querySelector("#startButton");
 const pauseButton = document.querySelector("#pauseButton");
@@ -11,30 +13,29 @@ const takeBreak = document.querySelector("#breakButton");
 const totalSessions = document.querySelector("#totalPomodoros");
 //sound effects
 const bellAlert = document.createElement("audio");
-bellAlert.setAttribute("src", "./bell-ringing-04.wav");
+bellAlert.setAttribute("src", "https://www.soundjay.com/misc/bell-ringing-04.mp3");
 const buttonSound = document.createElement("audio");
-buttonSound.setAttribute("src", "./button-1.wav");
-const clockTickingSound = document.createElement("audio");
-clockTickingSound.setAttribute("src", "./clock-ticking-1.wav");
+buttonSound.setAttribute("src", "https://www.soundjay.com/buttons/button-1.mp3");
+
 
 //functions
-
-function countdown() {
+//countdown clock engine
+function countdown() {  
   //Setting up the Pomodoro Clock Timer Elements
   let minutes = Math.floor(seconds / 60);
   let remainingSeconds = seconds % 60;
   let hours = Math.floor(seconds / 3600);
-  //Clock logic
+  //Clock display logic in HH:MM:SS
   minutes = minutes < 10 ? "0" + minutes : minutes;
   remainingSeconds =
     remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
   hours = hours < 10 ? "0" + hours : hours;
+  //output clock display logic to UI
   timeVal.innerText = hours + ":" + minutes + ":" + remainingSeconds;
   //clock starts
-  seconds--;
+  seconds--; 
 
-  //clock ticking sound lasts fully for duration of pomodoro session
-  // clockTickingSound.play();
+  //when countdown ends
   if (seconds < 0) {
     //if total completed Pomodoro sessions are more than 4 , then restart cycle count
     totalPSessions > 4 ? (totalPSessions = 0) : totalPSessions + 1;
@@ -42,41 +43,66 @@ function countdown() {
     totalPomodoros.innerText = totalPSessions;
     bellAlert.play();
     clearInterval(interval);
+    //re-enable start button
+    startButton.disabled = false;
   }
 }
+
+
+function pause(){
+  //stop countdown
+  clearInterval(interval);
+  //enable start button to resume countdown
+  startButton.disabled = false;
+  buttonSound.play();
+}
+
+//when clock has already been initiated once, resume countdown
+function resume(){
+  storedSeconds = seconds; //gather seconds into a new storage variable
+  storedSeconds--; //resume countdown and do not drop seconds
+}
+
+
 //event listeners
 startButton.addEventListener("click", function () {
-  //start clock by calling countdown method, each session -- 25 mins
-  seconds = 1500; //25 min session
-  buttonSound.play();
+  //Start countdown interval with calling countdown function and reduces time 1 second at a time
   interval = setInterval(countdown, 1000);
+  buttonSound.play();  
   totalPSessions += sessionCount + 1; //count each completed pomodoro session
+ //disable start button after first click to prevent multiple clicks that increases clock speed   
+  startButton.disabled = true;
+  takeBreak.disabled = true; //cannot take break until completion of Pomodoro cycle
+  resume(); //if paused and Start is clicked again, it should not restart the whole 25 mins, but resume where it was paused
 });
+    
 
 pauseButton.addEventListener("click", function () {
-  //pause clock
-  buttonSound.play();
-  clearInterval(interval);
+  //invoke pause function when clicked - pause clock
+  pause();
 });
 
 resetButton.addEventListener("click", function () {
-  seconds = 1500; //resetting to 25 mins
+  seconds = 1500; //reset 25min session
+  countdown(); //re-invoke function to set countdown clock
+  pause(); //keep paused
   buttonSound.play();
-  clearInterval(interval);
 });
 
 takeBreak.addEventListener("click", function () {
-  buttonSound.play();
+  
   //For big break after 4 total pomodoros is a 15 min break
   if (totalPSessions == 4) {
     seconds = 900;
     interval = setInterval(countdown, 1000);
-  } else if (totalPSessions == "") {
+    buttonSound.play();
+  } else if (totalPSessions === "" || totalPSessions === null || totalPSessions === 0) {
     //if  you didn't complete at least 1 pomodoro session, you cannot take a break yet
     takeBreak.disabled = true;
   } else {
     //less than 4 pomodoro sessions are individual 5 min breaks
     seconds = 300;
     interval = setInterval(countdown, 1000);
+    buttonSound.play();
   }
 });
